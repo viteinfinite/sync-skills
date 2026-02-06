3fa1524ee877f67a6c028fb6720ca774ce039081
Simone Civetta <viteinfinite@gmail.com>
2026-02-07 00:58:41 +0100

    feat: add support for 'user-invocable' and 'disable-model-invocation' fields

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 /**
  * Core frontmatter fields that should be preserved when syncing skills
  */
-export const CORE_FIELDS = ['name', 'description', 'license', 'compatibility', 'metadata', 'allowed-tools'] as const;
+export const CORE_FIELDS = ['name', 'description', 'license', 'compatibility', 'metadata', 'allowed-tools', 'user-invocable', 'disable-model-invocation'] as const;
 
 /** Common managed folder root */
 export const COMMON_DIR = '.sync-skills';
